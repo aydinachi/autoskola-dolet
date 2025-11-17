82506261a3c2162d44fdf1c96ab492c8bed78269
aydinachi <aydinachi@gmail.com>
2025-11-17 16:11:27 +0100

    Hero h1 change

--- src/components/UI/HeroSection/constants.ts
+++ src/components/UI/HeroSection/constants.ts
@@ -1,11 +1,11 @@
 // For desktop
-export const phrases = ['Postani vozač sa stilom.'];
+export const phrases = ['Postani vozač uz školu kojoj vjeruju generacije.'];
 export const paragraphPhrases = [
   "Profesionalna obuka, podrška i sigurnost na svakom koraku."
 ];
 
 // For mobile
-export const mobilePhrases = ['Postani vozač sa stilom.'];
+export const mobilePhrases = ['Postani vozač uz školu kojoj vjeruju generacije.'];
 export const mobileParagraphPhrases = [
   "Vaš partner za uspješno polaganje vozačkog ispita.",
   'Profesionalna obuka, iskusni instruktori.',
